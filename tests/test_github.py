9e1f032e314bbc9b5dfc90d075b27b09463af340
Sabine Maennel <sabine.maennel@gmail.com>
2023-01-20 17:49:46 +0100

    basic setup

---
+++ tests/test_github.py
@@ -0,0 +1,4 @@
+from tools import github
+
+def test_call_github_api():
+    github.call_github_api(with_auth=True, call_limit=2)
---
+++ tools/github.py
@@ -0,0 +1,36 @@
+import requests
+from dotenv import load_dotenv
+import pdb
+import os
+
+load_dotenv()
+
+
+#i'm providing username and API-Token in headers like
+url = 'https://api.github.com/repos/octocat/Spoon-Knife/issues'
+headers = {'Authorization': 'token ' + os.getenv('GITHUB_TOKEN')}
+
+login = requests.get('https://api.github.com/user', headers=headers)
+print(login.json())
+
+def f_auth(count):
+    res = requests.get(url, headers=headers)
+    print(f"{res.status_code} for {count}")
+    res.raise_for_status()
+
+
+def f_no_auth(count):
+    res = requests.get(url)
+    print(f"{res.status_code} for {count}")
+    res.raise_for_status()
+
+
+def call_github_api(with_auth=True, call_limit=100):
+    count = 0
+    if with_auth:
+        api_func = f_auth
+    else:
+        api_func = f_no_auth
+    for i in range(call_limit):
+        count += 1
+        api_func(count)
